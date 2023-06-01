33469120629fd0fdb803f1d9ef271406742871a1
Aneri Moradia <44959136+anerimoradia@users.noreply.github.com>
2023-06-01 00:51:54 -0700

    Update index.php
    added code for deleting an ID

--- index.php
+++ index.php
@@ -5,13 +5,23 @@ $database = new Database;
 
 $post = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
 
+if($_POST['delete']){
+    $delete_id = $_POST['delete_id'];
+    $database->query('DELETE FROM posts WHERE id = :id');
+    $database->bind(':id', $delete_id);
+    $database->execute();
+}
+
 if($post['submit']){
+    $id = $post['id'];
 	$title = $post['title'];
 	$body = $post['body'];
 
-	$database->query('INSERT INTO posts (title, body) VALUES(:title, :body)');
+	//$database->query('INSERT INTO posts (title, body) VALUES(:title, :body)');
+    $database->query('UPDATE posts SET title = :title, body = :body WHERE id = :id');
 	$database->bind(':title', $title);
 	$database->bind(':body', $body);
+    $database->bind(':id', $id);
 	$database->execute();
 	if($database->lastInsertId()){
 		echo '<p>Post Added!</p>';
@@ -23,6 +33,8 @@ $rows = $database->resultset();
 ?>
 <h1>Add Post</h1>
 <form method="post" action="<?php $_SERVER['PHP_SELF']; ?>">
+    <label>Post ID</label><br>
+    <input type="text" name="id" placeholder="Specify ID" /><br><br>
 	<label>Post Title</label><br />
 	<input type="text" name="title" placeholder="Add a Title..." /><br /><br />
 	<label>Post Body</label><br />
@@ -36,6 +48,11 @@ $rows = $database->resultset();
 	<div>
 		<h3><?php echo $row['title']; ?></h3>
 		<p><?php echo $row['body']; ?></p>
+        <br>
+        <form method="post" action="<?php $_SERVER['PHP_SELF']; ?>">
+            <input type="hidden" name="delete_id" value="<?php echo $row['id']; ?>">
+            <input type="submit" name="delete" value="Delete" />
+        </form>
 	</div>
 <?php endforeach; ?>
 </div>
