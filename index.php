3daec45ba2c8bbcaf7f95afda8973c4b1bedf4ec
Aneri Moradia <44959136+anerimoradia@users.noreply.github.com>
2023-05-31 22:06:19 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+class First{
+    public $id = 23;
+    protected $name = 'John Doe';
+
+    public function saySomething($word){
+        //echo 'Something...';   
+        echo $word;
+    }    
+}
+
+class Second extends First{
+    public function getName(){
+        echo $this->name;
+    }
+}
+
+$second = new Second;
+echo $second->getName();
+
+
+//echo $second->saySomething('Hello World');
