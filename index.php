ea7ddfbddc31201d52a3141f98b0d383a57911d2
Aneri Moradia <44959136+anerimoradia@users.noreply.github.com>
2023-06-01 00:32:52 -0700

    update index.php
    Added code to insert the data into database after clicking submit button

--- index.php
+++ index.php
@@ -1,23 +1,41 @@
-<?php
-
+<?php 
 require 'classes/Database.php';
 
 $database = new Database;
 
-$database->query('SELECT * FROM posts WHERE id = :id');
-$database->bind(':id', 1);      //you can change this two
+$post = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+if($post['submit']){
+	$title = $post['title'];
+	$body = $post['body'];
+
+	$database->query('INSERT INTO posts (title, body) VALUES(:title, :body)');
+	$database->bind(':title', $title);
+	$database->bind(':body', $body);
+	$database->execute();
+	if($database->lastInsertId()){
+		echo '<p>Post Added!</p>';
+	}
+}
+
+$database->query('SELECT * FROM posts');
 $rows = $database->resultset();
-//print_r($rows);
 ?>
-<h1>
-Posts
-</h1>
+<h1>Add Post</h1>
+<form method="post" action="<?php $_SERVER['PHP_SELF']; ?>">
+	<label>Post Title</label><br />
+	<input type="text" name="title" placeholder="Add a Title..." /><br /><br />
+	<label>Post Body</label><br />
+	<textarea name="body"></textarea><br /><br />
+	<input type="submit" name="submit" value="Submit" />
+</form>
+
+<h1>Posts</h1>
 <div>
 <?php foreach($rows as $row) : ?>
-    <div>
-        <h3> <?php echo $row['title']; ?></h3>
-        <p> <?php echo $row['body']; ?></p>
-    </div>
+	<div>
+		<h3><?php echo $row['title']; ?></h3>
+		<p><?php echo $row['body']; ?></p>
+	</div>
 <?php endforeach; ?>
 </div>
-
